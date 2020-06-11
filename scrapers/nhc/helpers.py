381bed0ffa8acaf0200244b1872fdae7a3bdc2dd
iflament <iflament.auc@gmail.com>
2020-06-11 12:09:31 +0000

    housekeeping cleanup

--- scrapers/nhc/helpers.py
+++ scrapers/nhc/helpers.py
@@ -1,3 +1,5 @@
+from sys import version_info
+import shutil
 import os
 from config import constants
 import mysql.connector
@@ -7,9 +9,74 @@ import subprocess
 import uuid
 from azure.storage.blob import BlobClient
 
+def cleanup_the_house():
 
-def find_files(myurl):
-    url = myurl
+    shutil.rmtree(f'{constants.data_dir}/')
+    shutil.rmtree(f'{constants.output_dir}/')
+
+def make_dirs(tropical_storms):
+
+    if os.path.exists(f'{constants.output_dir}/'):
+        shutil.rmtree(f'{constants.output_dir}/')
+    if os.path.exists(f'{constants.data_dir}/'):
+        shutil.rmtree(f'{constants.data_dir}/')
+
+    os.mkdir(f'{constants.output_dir}/')
+    os.mkdir(f'{constants.data_dir}/')
+    
+    for tropical_storm in tropical_storms:
+        os.mkdir(f'{constants.data_dir}/{tropical_storm.lower()}')
+        os.mkdir(f'{constants.data_dir}/{tropical_storm.lower()}/forecasts')
+        os.mkdir(f'{constants.data_dir}/{tropical_storm.lower()}/tracks')
+
+    return
+
+def validate_inputs(params):
+        errors = []
+
+        if not version_info[0] > 2:
+            errors.append('Python 3+ is required to run pipeline!')
+
+        if type(params['main_args']['upload']) != bool:
+            errors.append('upload argument should be True or False')
+
+        if type(params['main_args']['year']) != str:
+            errors.append('year argument should be a string')
+
+        if int(params['main_args']['year']) < 2008:
+            errors.append('year should be 2008 or later')
+
+        if int(params['main_args']['year']) > 2020:
+            errors.append('year should be 2020 or earlier')
+
+        if type(params['main_args']['storms_to_get']) != str:
+            errors.append('storms_to_get argument should be a string')
+
+        erroneous_storms = []
+        if params['main_args']['storms_to_get'] != '':
+            for storm in [params['main_args']['storms_to_get']]:
+                if storm.upper() not in params['all_nhc_storms']:
+                    erroneous_storms.append(storm)
+                    errors.append(f"You are requesting data for storms that don't exist in NHC database: {erroneous_storms}!")
+
+        if type(params['main_args']['scrapetype']) != str:
+            errors.append("scrapetype should be a string, either 'all' or 'latest' ")
+
+        if params['main_args']['scrapetype'] not in ["latest","all"]:
+            errors.append("scrapetype should be either 'all' or 'latest' ")
+
+        if type(params['main_args']['odds_container']) != str:
+            errors.append('odds_container should be a string')
+
+        if params['main_args']['odds_container'] not in ["odds", "testcontainer", "nhc", "demos"]:
+            errors.append('odds_container chosen is not allowed, please use "odds", "testcontainer", "nhc" or "demos" ')
+        if errors:
+            print(f'Error: {errors}')
+
+        return errors
+
+
+def find_files(url):
     soup = BeautifulSoup(requests.get(url).text, features="html.parser")
 
     links = []
@@ -19,8 +86,8 @@ def find_files(myurl):
     return links
 
 
-def get_active_storms(myurl):
-    soup = BeautifulSoup(requests.get(myurl).text, features="html.parser")
+def get_active_storms(url):
+    soup = BeautifulSoup(requests.get(url).text, features="html.parser")
 
     contents = []
     for element in soup.find_all('td'):
@@ -36,8 +103,8 @@ def get_active_storms(myurl):
     return active_storms
 
 
-def get_storms(myurl):
-    soup = BeautifulSoup(requests.get(myurl).text, features="html.parser")
+def get_storms(url):
+    soup = BeautifulSoup(requests.get(url).text, features="html.parser")
 
     contents = []
     for element in soup.find_all('td'):
@@ -53,9 +120,9 @@ def get_storms(myurl):
     return output
 
 
-def get_links(myurl):
+def get_links(url):
 
-    list_of_links = find_files(myurl)
+    list_of_links = find_files(url)
     forecasts = []
     tracks = []
     for link in list_of_links:
@@ -97,8 +164,7 @@ def convert_to_geojson(params, scrapetype, directory, storm):
             nums.append(newnum)
         
         if nums:
-            # get latest num, sort ascending
-            maxnums = max(list(set(nums))) 
+            maxnums = max(list(set(nums)))
             for myfile in files:
                 if '.shp' in myfile:
                     if str(maxnums) in str(myfile.split('/')[-1].split("_")[0].split("-")[-1]):
--- scrapers/nhc/nhc.py
+++ scrapers/nhc/nhc.py
@@ -1,9 +1,7 @@
-from sys import version_info
 from config import constants
 import argparse
 import yaml
 import helpers
-import shutil
 import os
 
 parser = argparse.ArgumentParser(description='pipeline')
@@ -32,42 +30,57 @@ class Pipeline():
         print(f" Collecting {self.scrapetype} data... ")
  
     def run(self):
+        
         with open(constants.creds, 'r') as f:
             creds = dict(yaml.safe_load(f.read()))
         with open(constants.params, 'r') as f:
             params = dict(yaml.safe_load(f.read()))
- 
-        errors = self._validate_inputs(params)
+            params['main_args'] = {'upload': self.upload, 'year': self.year, 'storms_to_get': self.storms_to_get, 'scrapetype': self.scrapetype, 'odds_container': self.odds_container}
+
+        errors = helpers.validate_inputs(params)
+        
         if not errors: 
             if self.storms_to_get != '':
                 tropical_storms = [self.storms_to_get]
             else:
-                tropical_storms = helpers.get_storms(myurl=f"https://www.nhc.noaa.gov/gis/archive_forecast.php?year={self.year}")    
+                tropical_storms = helpers.get_storms(url=f"https://www.nhc.noaa.gov/gis/archive_forecast.php?year={self.year}")    
 
-            active_tropical_storms = helpers.get_active_storms(myurl="https://www.nhc.noaa.gov/cyclones/")
+            active_tropical_storms = helpers.get_active_storms(url="https://www.nhc.noaa.gov/cyclones/")
             
             if all(value is False for value in active_tropical_storms.values()):
                 print(" There are currently no active tropical storms in the Atlantic, Central North Pacific or Eastern North Pacific at this time.")
                 active_tropical_storms = ["No Active Tropical Storms"]
              
-            # update MRT with list of all active tropical storms
             helpers.insert_storms_in_mrt(creds, active_tropical_storms)
             
             if self.scrapetype == 'latest':
                 tropical_storms = active_tropical_storms
                 helpers.insert_storms_in_mrt(creds, active_tropical_storms)
-            
+           
+            helpers.make_dirs(tropical_storms)
+
             if not tropical_storms == ["No Active Tropical Storms"]:
                 for tropical_storm in tropical_storms:
-                    os.mkdir(f'{constants.data_dir}/{tropical_storm.lower()}')
-                    os.mkdir(f'{constants.data_dir}/{tropical_storm.lower()}/forecasts')
-                    os.mkdir(f'{constants.data_dir}/{tropical_storm.lower()}/tracks')
                     code = params[str(self.year)][tropical_storm.upper()]['code']
-                    forecasts, tracks = helpers.get_links(myurl=f"https://www.nhc.noaa.gov/gis/archive_forecast_results.php?id={code}&year={self.year}&name=Tropical%{self.year[0:2]}Storm%{self.year[2:4]}{tropical_storm}")
-                    forecasts = helpers.get_data_from_url(self.upload, to_download=forecasts, directory=f'{constants.data_dir}/{tropical_storm.lower()}/forecasts')
-                    tracks = helpers.get_data_from_url(self.upload, to_download=tracks, directory=f'{constants.data_dir}/{tropical_storm.lower()}/tracks')
-                    forecasts = helpers.convert_to_geojson(params, self.scrapetype, directory=f'{constants.data_dir}/{tropical_storm.lower()}/forecasts', storm=tropical_storm.lower())
-                    tracks = helpers.convert_to_geojson(params, self.scrapetype, directory=f'{constants.data_dir}/{tropical_storm.lower()}/tracks', storm=tropical_storm.lower())
+                    forecasts, tracks = helpers.get_links(url=f"https://www.nhc.noaa.gov/gis/archive_forecast_results.php?id={code}&year={self.year}&name=Tropical%{self.year[0:2]}Storm%{self.year[2:4]}{tropical_storm}")
+                    
+                    forecasts = helpers.get_data_from_url(self.upload, 
+                            to_download=forecasts, 
+                            directory=f'{constants.data_dir}/{tropical_storm.lower()}/forecasts')
+                    
+                    tracks = helpers.get_data_from_url(self.upload, 
+                            to_download=tracks, 
+                            directory=f'{constants.data_dir}/{tropical_storm.lower()}/tracks')
+                    
+                    forecasts = helpers.convert_to_geojson(params, 
+                            self.scrapetype, 
+                            directory=f'{constants.data_dir}/{tropical_storm.lower()}/forecasts', 
+                            storm=tropical_storm.lower())
+                    
+                    tracks = helpers.convert_to_geojson(params, 
+                            self.scrapetype, 
+                            directory=f'{constants.data_dir}/{tropical_storm.lower()}/tracks', 
+                            storm=tropical_storm.lower())
                 
                     if self.upload:
                         for datatype in [forecasts, tracks]:
@@ -89,61 +102,10 @@ class Pipeline():
                                     containerName = self.odds_container, 
                                     blobName = datafile)
 
-    def _validate_inputs(self, params):
-        errors = []
-
-        if not version_info[0] > 2:
-            errors.append('Python 3+ is required to run pipeline!')
-
-        if type(self.upload) != bool:
-            errors.append('upload argument should be True or False')
-        
-        if type(self.year) != str:
-            errors.append('year argument should be a string')
-       
-        if int(self.year) < 2008:
-            errors.append('year should be 2008 or later')
-
-        if int(self.year) > 2020:
-            errors.append('year should be 2020 or earlier')
-
-        if type(self.storms_to_get) != str:
-            errors.append('storms_to_get argument should be a string')
-       
-        erroneous_storms = []
-        if self.storms_to_get != '':
-            for storm in [self.storms_to_get]:
-                if storm.upper() not in params['all_nhc_storms']:
-                    erroneous_storms.append(storm)
-                    errors.append(f"You are requesting data for storms that don't exist in NHC database: {erroneous_storms}!")
-
-        if type(self.scrapetype) != str:
-            errors.append("scrapetype should be a string, either 'all' or 'latest' ")
-                
-        if self.scrapetype not in ["latest","all"]:
-            errors.append("scrapetype should be either 'all' or 'latest' ")
-        
-        if type(self.odds_container) != str:
-            errors.append('odds_container should be a string')
-                
-        if self.odds_container not in ["odds", "testcontainer", "nhc", "demos"]:
-            errors.append('odds_container chosen is not allowed, please use "odds", "testcontainer", "nhc" or "demos" ')
-        if errors:
-            print(f'Error: {errors}')
-            errors = True
-            
-        return errors
+            helpers.cleanup_the_house()
 
 
 if __name__ == "__main__":
-
-    if os.path.exists(f'{constants.output_dir}/'):
-        shutil.rmtree(f'{constants.output_dir}/')
-    if os.path.exists(f'{constants.data_dir}/'):
-        shutil.rmtree(f'{constants.data_dir}/')
-
-    os.mkdir(f'{constants.output_dir}/')
-    os.mkdir(f'{constants.data_dir}/')
 
     pipeline = Pipeline(
         loggers = args.loggers,
@@ -155,5 +117,3 @@ if __name__ == "__main__":
         )
 
     pipeline.run()
-    shutil.rmtree(f'{constants.data_dir}/')
-    shutil.rmtree(f'{constants.output_dir}/')
