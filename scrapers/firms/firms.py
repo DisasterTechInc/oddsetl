f86b4807fd10cb4cbaddd9b2490d31b24d5110e5
iflament <iflament.auc@gmail.com>
2020-07-07 14:54:45 +0000

    firms updates

--- scrapers/firms/firms.py
+++ scrapers/firms/firms.py
@@ -14,56 +14,59 @@ logging.config.dictConfig(logger_config)
 parser = argparse.ArgumentParser(description='pipeline')
 parser.add_argument('--loggers', type=bool, default=True)
 parser.add_argument('--upload', type=bool, default=True)
-parser.add_argument('--odds_container', type=str, default='firms')
+#parser.add_argument('--odds_container', type=str, default='firms')
 args = parser.parse_args()
 
 
 class Pipeline():
-    def __init__(self, loggers, upload, odds_container):
+    def __init__(self, loggers, upload):
         self.loggers = loggers
         self.upload = upload
-        self.odds_container = odds_container
         logger.propagate = self.loggers
        
     def run(self):
 
         with open(constants.creds, 'r') as f:
             creds = dict(yaml.safe_load(f.read()))
-        
-        imap_url = 'imap.googlemail.com'
-        con = imaplib.IMAP4_SSL(imap_url)
        
+        con = imaplib.IMAP4_SSL('imap.googlemail.com')
+        
+        urls = ['https://firms.modaps.eosdis.nasa.gov/data/active_fire/c6/shapes/zips/MODIS_C6_Global_24h.zip',
+                'https://firms.modaps.eosdis.nasa.gov/data/active_fire/suomi-npp-viirs-c2/shapes/zips/SUOMI_VIIRS_C2_Global_24h.zip',
+                'https://firms.modaps.eosdis.nasa.gov/data/active_fire/noaa-20-viirs-c2/shapes/zips/J1_VIIRS_C2_Global_24h.zip']
         try:
-            firmsfiles = helpers.get_message(con, creds)
-            if self.upload:
-                for firmsfile in firmsfiles:
-                    firmsfile = f"{firmsfile.split('/')[-1].split('.')[0]}.geojson"
-                    alert = f"{firmsfile.split('_')[-1].split('.')[0]}" 
-                    print(f"Importing latest {firmsfile} rapid alerts to odds db")
-                    helpers.store_blob_in_odds(logger, 
-                        datafile = f"{constants.output_dir}/{firmsfile}", 
-                        token = creds['TOKEN'], 
-                        connectionString = creds['connectionString'], 
-                        containerName = self.odds_container, 
-                        blobName = f"firms_{alert}.geojson") # todo: inject date into data 
-        
-        except:
+            helpers.get_active_wildfire(urls=urls, 
+                    creds=creds,
+                    upload = self.upload,
+                    input_dir=constants.activefires_input, output_dir=constants.activefires_output)
+        except Exception:
             print(sys.exc_info()[1])
-            sys.exit(1)
-        
+
+        #try:
+        #    helpers.get_nrt_fire_alerts(con=con, 
+        #            creds=creds, 
+        #            upload=self.upload, 
+        #            input_dir=constants.alerts_input, 
+        #            output_dir=constants.alerts_output)
+        #except Exception:
+        #    print(sys.exc_info()[1])
+
         helpers.cleanup_the_house()
 
 if __name__ == "__main__":
     
-    if not os.path.exists(f'{constants.output_dir}/'):
-        os.mkdir(f'{constants.output_dir}/')
-    if not os.path.exists(f'{constants.data_dir}/'):
-        os.mkdir(f'{constants.data_dir}/')
+    if not os.path.exists(f'{constants.activefires_input}/'):
+        os.mkdir(f'{constants.activefires_input}/')
+    if not os.path.exists(f'{constants.activefires_output}/'):
+        os.mkdir(f'{constants.activefires_output}/')
+    if not os.path.exists(f'{constants.alerts_input}/'):
+        os.mkdir(f'{constants.alerts_input}/')
+    if not os.path.exists(f'{constants.alerts_output}/'):
+        os.mkdir(f'{constants.alerts_output}/')
 
     pipeline = Pipeline(
         loggers = args.loggers,
         upload = args.upload, 
-        odds_container = args.odds_container
         )
 
     pipeline.run()
--- scrapers/firms/helpers.py
+++ scrapers/firms/helpers.py
@@ -1,4 +1,5 @@
 import sys
+import requests, zipfile, io
 from azure.storage.blob import BlobClient
 import shutil
 import imaplib, os
@@ -6,7 +7,7 @@ import email
 import subprocess
 from config import logger_config, constants
 
-def get_message(con, creds):
+def get_message(con, creds, input_dir, output_dir):
     (retcode, capabilities) = con.login(creds['email_user'],creds['email_pass'])
     con.select(readonly=1)
     (retcode, messages) = con.search(None, '(ALL)') #UNSEEN
@@ -14,45 +15,117 @@ def get_message(con, creds):
     id_list = mail_ids.split()
     
     firmsfiles = []
+    dates = {}
     for num in id_list:
-        typ, data = con.fetch(num, '(RFC822)' )
+        typ, data = con.fetch(num, '(RFC822)')
         raw_email = data[0][1]
         raw_email_string = raw_email.decode('utf-8')
         email_message = email.message_from_string(raw_email_string)
-        #date = str(email_message).split("(PDT)")[0].split(";")[-1].replace(" ",'').split(',')[-1].split('-')[0]
+        # filter mail for spam - only download FIRMS alerts
+        subject = str(email_message).split("Subject: ", 1)[1].split("\nTo:", 1)[0]
+        dates['date'] = str(email_message).split("(PDT)")[0].split(";")[-1].replace(" ",'').split(',')[-1].split('-')[0]  
+        dates['day'] = dates['date'].split(':')[0]
+        dates['email_message'] = email_message
+        dates['num'] = num
+        dates['subject'] = subject
+    
+    import pdb;pdb.set_trace()
+    for message in dates.items():
+        email_message = dates['email_message']
+        if 'FIRMS Rapid Alert' not in subject:
+            print(subject)
+            print('POSSIBLE EMAIL SPAM, email subject: {subject}')
+            sys.exit()
+        else:
+            for part in email_message.walk():
+                if part.get_content_maintype() == 'multipart':
+                    continue
+                if part.get('Content-Disposition') is None:
+                    continue
 
-        for part in email_message.walk():
-            if part.get_content_maintype() == 'multipart':
-                continue
-            if part.get('Content-Disposition') is None:
-                continue
-
-            fileName = part.get_filename()
-            if bool(fileName):
-                filePath = f'{constants.data_dir}/firms_{fileName}'
-                if not os.path.isfile(filePath) :
-                    fp = open(filePath, 'wb')
-                    fp.write(part.get_payload(decode=True))
-                    fp.close()
-                #subject = str(email_message).split("Subject: ", 1)[1].split("\nTo:", 1)[0]
-                convert_to_geojson(myfile=filePath)
-                firmsfiles.append(f"firms_{fileName.split('.')[0]}")
+                fileName = part.get_filename()
+                if bool(fileName):
+                    filePath = f'{input_dir}/firms_{fileName}'
+                    if not os.path.isfile(filePath) :
+                        fp = open(filePath, 'wb')
+                        fp.write(part.get_payload(decode=True))
+                        fp.close()
+                    print(f'Converting file {filePath}')
+                    convert_to_geojson(inputfile=filePath, output_dir=output_dir)
+                    firmsfiles.append(f"firms_{fileName.split('.')[0]}")
 
     return firmsfiles 
 
-def cleanup_the_house():
-    shutil.rmtree(f'{constants.data_dir}/')
-    shutil.rmtree(f'{constants.output_dir}/')
-
-def convert_to_geojson(myfile):
-    """Convert a kml or shapefile to a geojson file, and output in corresponding datadir."""
-    bashCommand = f"k2g {myfile} {constants.output_dir}"
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-
+def get_nrt_fire_alerts(con, creds, upload, input_dir, output_dir):
+    
+    firmsfiles = get_message(con, creds, input_dir, output_dir)
+    if upload:
+        for firmsfile in firmsfiles:
+            firmsfile = f"{firmsfile.split('/')[-1].split('.')[0]}.geojson"
+            alert = f"{firmsfile.split('_')[-1].split('.')[0]}"
+            print(f"Importing latest {firmsfile} rapid alerts to odds db")
+            store_blob_in_odds(datafile = f"{output_dir}/{firmsfile}",
+                token = creds['TOKEN'],
+                connectionString = creds['connectionString'],
+                containerName = 'firms',
+                blobName = f"firms_{alert}.geojson") # todo: inject date into data
     return
 
-def store_blob_in_odds(logger, datafile, token, connectionString, containerName, blobName):
+def get_active_wildfire(urls, creds, upload, input_dir, output_dir):
+    """Given a list of links to download, get data from urls."""
+
+    for url in urls:
+        status_code = requests.get(url).status_code
+        if status_code == 404:
+            print(f'Url status code {status_code}: {url}')
+            status_code = requests.get(url).status_code
+        if status_code == 200:
+            try:
+                name = url.split("/")[-1].split(".")[0]
+                r = requests.get(url)
+                z = zipfile.ZipFile(io.BytesIO(r.content))
+                z.extractall(f'{input_dir}/{name}')
+            except Exception:
+                print(f'Could not retrieve {url}')
+            
+            files = os.listdir(f"{input_dir}/{name}")
+            
+            files = os.listdir(f"{input_dir}/{name}")
+            
+            files = os.listdir(f"{input_dir}/{name}")
+            files = [f for f in os.listdir(f"{input_dir}/{name}") if f.endswith('.shp')] 
+            for firefile in files:
+                datatype = '_'.join(firefile.split("_")[0:2])
+                print(firefile)
+                convert_to_geojson(inputfile=f"{input_dir}/{name}/{firefile}", output_dir=output_dir)
+                if upload:
+                    firefile = f"{firefile.split('/')[-1].split('.')[0]}.geojson"
+                    store_blob_in_odds(datafile = f"{output_dir}/{firefile}",
+                    token = creds['TOKEN'],
+                    connectionString = creds['connectionString'],
+                    containerName = 'firms',
+                    blobName = f"firms_{datatype}_active_wildfire_24h.geojson") # todo: inject date into data
+    return
+
+def cleanup_the_house():
+    shutil.rmtree(f'{constants.alerts_input}')
+    shutil.rmtree(f'{constants.alerts_output}')
+    shutil.rmtree(f'{constants.activefires_input}')
+    shutil.rmtree(f'{constants.activefires_output}')
+
+def convert_to_geojson(inputfile, output_dir):
+    """Convert a kml or shapefile to a geojson file, and output in corresponding datadir."""
+    
+    bashCommand = None 
+    if "kml" in inputfile:
+        bashCommand = f"k2g {inputfile} {output_dir}"
+    elif "shp" in inputfile:
+        bashCommand = f"ogr2ogr -f GeoJSON {output_dir}/{inputfile.split('/')[-1].split('.')[0]}.geojson {inputfile}"    
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    return
+
+def store_blob_in_odds(datafile, token, connectionString, containerName, blobName):
     """Store json files in db."""
 
     blob = BlobClient.from_connection_string(conn_str=connectionString, container_name=containerName, blob_name=blobName)
@@ -60,4 +133,6 @@ def store_blob_in_odds(logger, datafile, token, connectionString, containerName,
         blob.upload_blob(f, overwrite=True)
 
     headers = {"Authorization": "Bearer %s" %token, "content-type":"application/json"}
-    logger.info(f"Upload successful to odds.{containerName}: {blobName}")
+    print(f"Upload successful to odds.{containerName}: {blobName}")
+
+
