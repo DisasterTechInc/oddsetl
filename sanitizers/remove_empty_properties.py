503053f0f4cac009f84dda106368782f4ed1bb04
iflament <iflament.auc@gmail.com>
2020-07-11 11:36:31 +0000

    sanitizers initial commit

---
+++ sanitizers/remove_empty_properties.py
@@ -0,0 +1,17 @@
+
+def remove_empty_properties(geojson_input):
+    
+    new_features = []
+    properties = {}
+    for feature in geojson_input['features']:
+        for k, v in feature['properties'].items():
+            myfeature = {'type': 'Feature', 'properties': {}}
+            if v != '':  
+                properties[k] = feature['properties'][k]
+        
+            myfeature['properties'] = properties
+            new_features.append(myfeature)
+
+    geojson_input["features"] = new_features
+
+    return geojson_input 
